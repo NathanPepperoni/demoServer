162acc836b5b7c66cbe6512bfbb50e3d78bb8a4a
EC2 Default User <ec2-user@ip-172-31-46-112.us-east-2.compute.internal>
2019-01-12 00:22:37 +0000

    s

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const path = require('path');
 const exec = require('child_process');
 const app = express();
 const port = 8080;
+var AWS = require('aws-sdk');
+AWS.config.update({region: 'us-east-2'});
 
 app.use(express.static('public'));
 
@@ -27,6 +29,32 @@ app.listen(port, function () {
   console.log(`Example app listening on port ${port}.`);
 });
 
-function shutdown(callback){
-  exec('shutdown now', function(error, stdout, stderr){ callback(stdout); });
-}
+ var params = {
+  InstanceIds: [
+     "i-058832be9447e4e5e"
+  ]
+ };
+
+ec2 = new AWS.EC2();
+
+ec2.stopInstances(params, function(err, data) {
+   if (err) console.log(err, err.stack); // an error occurred
+   else     console.log(data);           // successful response
+   /*
+ *    data = {
+ *        StoppingInstances: [
+ *               {
+ *                     CurrentState: {
+ *                            Code: 64, 
+ *                                   Name: "stopping"
+ *                                         }, 
+ *                                               InstanceId: "i-1234567890abcdef0", 
+ *                                                     PreviousState: {
+ *                                                            Code: 16, 
+ *                                                                   Name: "running"
+ *                                                                         }
+ *                                                                              }
+ *                                                                                  ]
+ *                                                                                     }
+ *                                                                                        */
+ });
